69762b57dfdbe66995921858ae74f84da45e79a6
Rithishkumar07 <129845337+Rithishkumar07@users.noreply.github.com>
2023-06-03 18:53:14 +0530

    Create solution.cpp

---
+++ dp/Coin Change II/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,0));
+        dp[0][0]=1;
+        for(int i=1;i<=coins.size();i++){
+            for(int j=0;j<=amount;j++){
+                if(coins[i-1]>j)dp[i][j]=dp[i-1][j];
+                else{
+                    dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]];
+                }
+            }
+        }
+        return dp[coins.size()][amount];
+    }
+};
